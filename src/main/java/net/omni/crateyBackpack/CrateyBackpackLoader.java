f662477f825973f516f28a4ee351dccf33b05324
domninos <domninosdev@gmail.com>
2026-07-06 14:20:11 +0800

    Initial Commit

---
+++ src/main/java/net/omni/crateyBackpack/CrateyBackpackLoader.java
@@ -0,0 +1,12 @@
+package net.omni.crateyBackpack;
+
+import io.papermc.paper.plugin.loader.PluginClasspathBuilder;
+import io.papermc.paper.plugin.loader.PluginLoader;
+
+class CrateyBackpackLoader implements PluginLoader {
+
+    @Override
+    public void classloader(final PluginClasspathBuilder builder) {
+        // Add dynamically loaded libraries here
+    }
+}
